842b157ea3b04e228cc17234b6d5ccfa93e02260
root <mhbabo95@gmail.com>
2024-07-06 15:00:41 +0000

    Added pulumi

---
+++ __main__.py
@@ -0,0 +1,12 @@
+"""An AWS Python Pulumi program"""
+
+import pulumi
+import pulumi_aws as aws
+
+
+
+vpc = aws.ec2.Vpc("my-vpc",
+    cidr_block="10.10.0.0/16",
+    enable_dns_hostnames=True,
+    enable_dns_support=True,
+)
